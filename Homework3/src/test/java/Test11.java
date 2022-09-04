d6bc6302c3635b43e041e6c4db77594cb8b094fc
andreyastafyev <a-astafiev@mail.ru>
2022-09-04 12:11:20 +0300

    last update 04.09.22

--- Homework3/src/test/java/Test11.java
+++ Homework3/src/test/java/Test11.java
@@ -5,9 +5,6 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.Screenshot;
-import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
@@ -19,8 +16,11 @@ private WebDriver driver;
 private String baseUrl;
 @BeforeClass(alwaysRun = true)
 public void setUp() throws Exception {
-System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
-driver = new ChromeDriver();
+//System.setProperty("webdriver.chrome.driver", "src/chromedriver.exe");
+//driver = new ChromeDriver();
+System.setProperty("webdriver.chrome.driver", "C:/Users/a-ast/work/git/andreyastafyev-QA/Homework3/src/test/chromedriver.exe");
+ driver = new ChromeDriver();
+driver.get("http://www.google.com/");
 baseUrl = "https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d";
 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 driver.manage().window().fullscreen();}
